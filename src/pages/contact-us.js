f7ca2ff45421b94980c14e6c9dd8fe9bc3ce8d97
Joshua Isaac <hello.joshua@hotmail.com>
2020-03-29 14:28:43 -0400

    remove submit handler

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -76,7 +76,6 @@ const ContactUs = (props) => {
                 <RichText render={props.data.prismic.allContact_pages.edges[0].node.form_title} />
                 <RichText render={props.data.prismic.allContact_pages.edges[0].node.form_description} />
                 <Form 
-                    onSubmit={e => e.preventDefault()}
                     name="contact-us"
                     method="POST"
                     data-netlify="true"
